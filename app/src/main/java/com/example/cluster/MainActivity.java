9157496f155950f05577fc6845338c8dca9f5566
Hakim Bwanika <b4hakim@gmail.com>
2020-10-30 20:09:06 +0300

    Second Commit

--- app/src/main/java/com/example/cluster/MainActivity.java
+++ app/src/main/java/com/example/cluster/MainActivity.java
@@ -1,44 +1,74 @@
 package com.example.cluster;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
-import java.security.Permission;
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView messagesRecyclerview;
+    private static final int READ_SMS_PERMISSIONS_REQUEST = 1982;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
-        readList();
+        messagesRecyclerview = findViewById(R.id.message_list);
+        messagesRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSms();
+        } else {
+            refreshSmsInbox();
+        }
     }
 
-    public List<String> readSMS(){
-        List<String> sms = new ArrayList<String>();
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null,null, null, null);
-        while (cursor.moveToNext()) {
-            String address = cursor.getString(cursor.getColumnIndex("address"));
-            String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
-            sms.add("Number: " + address + " .Message: " + body);
+    private void refreshSmsInbox() {
+        MessageLab messageLab = MessageLab.get(this);
+        List<Message> messages = messageLab.getMessages();
+
+        MessageAdapter arrayAdapter = new MessageAdapter(messages);
+        messagesRecyclerview.setAdapter(arrayAdapter);
+
+    }
+
+    private void getPermissionToReadSms() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)!= PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_SMS)) {
+                    Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_SMS}, READ_SMS_PERMISSIONS_REQUEST);
         }
-        return sms;
     }
-    private void readList() {
-        List smsList = readSMS();
-        String smslist = smsList.toString();
-        TextView mReadTextView = findViewById(R.id.readTextView);
-        mReadTextView.setText(smslist);
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+                refreshSmsInbox();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
+
 }
