7bd8043669d3d50c82ee40c306d56b822184719c
Hakim Bwanika <b4hakim@gmail.com>
2020-11-06 22:39:40 +0300

    Second Commit

--- app/src/main/java/com/example/cluster/MessageCursorWrapper.java
+++ app/src/main/java/com/example/cluster/MessageCursorWrapper.java
@@ -4,21 +4,57 @@ import android.database.Cursor;
 import android.database.CursorWrapper;
 import android.util.Log;
 
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MessageCursorWrapper extends CursorWrapper {
 
     private MessageUtils mUtils;
+    private SettingsActivity mActivity;
+    private PieEntry pieEntry;
+    private PieDataSet mPieDataSet;
+    private List<Integer> mList;
+    private List<Integer> paidList;
+    private List<Integer> sentList;
+    private List<Integer> withdrawalList;
+    private int sum = 0;
 
 
     public MessageCursorWrapper(Cursor cursor) {
         super(cursor);
+        mList = new ArrayList<>();
+        paidList = new ArrayList<>();
+        sentList = new ArrayList<>();
+        withdrawalList = new ArrayList<>();
+
     }
 
-    public Message getMessage() {
+    public Message getSingleMessage() {
+        String idString = getString(getColumnIndex("_id"));
+        String body = getString(getColumnIndex("body"));
+        String address = getString(getColumnIndex("address"));
+        long date = getLong(getColumnIndex("date"));
+
+        Message message = new Message(idString);
+        message.setAddress(address);
+        message.setBody(body.trim());
+        Date mmDate = new Date(date);
+        SimpleDateFormat sfd = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss", Locale.US);
+        String dateFormat = sfd.format(mmDate);
+        message.setDate(dateFormat);
+
+        getAccountBalance(message);
+
+        return message;
+    }
+
+    public Message getMessages() {
         String idString = getString(getColumnIndex("_id"));
         String body = getString(getColumnIndex("body"));
         String address = getString(getColumnIndex("address"));
@@ -33,6 +69,8 @@ public class MessageCursorWrapper extends CursorWrapper {
         message.setDate(dateFormat);
         getPaidAmount(message);
 
+        Log.d("Summation", String.valueOf(sum));
+
         return message;
     }
 
@@ -40,67 +78,106 @@ public class MessageCursorWrapper extends CursorWrapper {
         String[] bodyChunks = body.getBody().split(" ");
         String transaction;
         String balance;
-        int amount;
+        String amount;
+        String amountSent;
         mUtils = new MessageUtils();
+        mActivity = new SettingsActivity();
 
-        if (body.getBody().contains("Topup")) {
-            transaction = bodyChunks[3].replaceAll(",","").trim();
-            body.setAmount(transaction.replaceAll("[.]", "").trim());
-            balance = bodyChunks[9].replaceAll(",","").trim();
-            body.setBalance("Topped Up - " +balance);
-        }
+
         if (body.getBody().contains("sent")) {
             transaction = bodyChunks[4].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
-            balance = bodyChunks[15].replaceAll(",","").trim();
-            body.setBalance("Sent - " +balance);
+            amountSent = transaction.replaceAll(",","");
+            body.setAmount("You sent - UGX " + amountSent);
+
+            balance = bodyChunks[16].replaceAll("[.]","").trim();
+            body.setBalance("Balance - UGX " + balance.replaceAll(",",""));
+
+            int newInt = Integer.parseInt(amountSent);
+            sentList.add(newInt);
+            for (int i : mList) {
+                sum = sum + i;
+            }
+            body.setSentList(sentList);
+
+        }
+
+        if (body.getBody().contains("withdrawal") && !body.getBody().contains("initiated")) {
+            transaction = bodyChunks[4].replaceAll("[.]", "").trim();
+            amount = transaction.replaceAll(",","");
+            body.setAmount("Withdrawal - UGX " + amount);
+            balance = bodyChunks[14].replaceAll(",","").trim();
+            body.setBalance("Balance - UGX " + balance.replaceAll("[.]", "").trim());
+
+            int newInt = Integer.parseInt(amount);
+            withdrawalList.add(newInt);
+            for (int i : mList) {
+                sum = sum + i;
+            }
+            body.setWithdrawalList(withdrawalList);
+
+        }
+
+        if (body.getBody().contains("Paid") && !body.getBody().contains("UMEME")) {
+            transaction = bodyChunks[2].replaceAll("[.]", "").trim();
+            amount = transaction.replaceAll(",","");
+            body.setAmount("Paid - UGX " + amount);
+            balance = bodyChunks[13].replaceAll(",","").trim();
+            body.setBalance("Balance - UGX " + balance.replaceAll("[.]", "").trim());
+
+            int newInt = Integer.parseInt(amount);
+            paidList.add(newInt);
+            for (int i : mList) {
+                sum = sum + i;
+            }
+            body.setPaidList(paidList);
+
+        }
+    }
+
+    private void getAccountBalance(Message body) {
+
+        String[] bodyChunks = body.getBody().split(" ");
+        String transaction;
+        String balance;
+        String amount;
+
+        if (body.getBody().contains("Topup") && body.getBody().contains("Balance")) {
+            balance = bodyChunks[9].replaceAll(",","").trim();
+            body.setBalance("Your Account Balance - UGX " + balance);
+        }
+        if (body.getBody().contains("sent")) {
+            balance = bodyChunks[16].replaceAll("[.]","").trim();
+            body.setBalance("Your Account Balance - UGX " + balance.replaceAll(",",""));
         }
 
         if (body.getBody().contains("deposit")) {
-            transaction = bodyChunks[4].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
             balance = bodyChunks[10].replaceAll(",","").trim();
-            body.setBalance("Deposited - " +balance.replaceAll("[.]", "").trim());
+            body.setBalance("Your Account Balance - UGX " + balance.replaceAll("[.]", "").trim());
         }
 
-        if (body.getBody().contains("withdrawal")) {
-            transaction = bodyChunks[4].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
-            balance = bodyChunks[15].replaceAll(",","").trim();
-            body.setBalance("Withdrawn - " + balance);
+        if (body.getBody().contains("withdrawal") && !body.getBody().contains("initiated")) {
+            balance = bodyChunks[14].replaceAll(",","").trim();
+            body.setBalance("Your Account Balance - UGX " + balance.replaceAll("[.]", "").trim());
         }
 
         if (body.getBody().contains("received")) {
-            transaction = bodyChunks[4].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
-
+            balance = bodyChunks[13].replaceAll(",","").trim();
+            body.setBalance("Your Account Balance - UGX " + balance.replaceAll("[.]", "").trim());
         }
 
         if (body.getBody().contains("payment")) {
-            transaction = bodyChunks[4].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
             balance = bodyChunks[13].replaceAll(",","").trim();
             if (balance.endsWith(".Thank")) {
                 balance = balance.substring(0, balance.length() - 6);
-                body.setBalance("Payment - " + balance);
+                body.setBalance("Your Account Balance - UGX " + balance);
             }
-
-        }
-
-        if (body.getBody().contains("Dear")) {
-            transaction = bodyChunks[6].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
-        }
-
-        if (body.getBody().contains("3GB")) {
-            transaction = "10000";
-            body.setAmount(transaction);
         }
 
         if (body.getBody().contains("Paid")) {
-            transaction = bodyChunks[2].replaceAll("[.]", "").trim();
-            body.setAmount(transaction.replaceAll(",",""));
+            balance = bodyChunks[8].replaceAll(",","").trim();
+            body.setBalance("Your Account Balance - UGX " + balance.replaceAll("[.]", "").trim());
         }
+
     }
 
 }
